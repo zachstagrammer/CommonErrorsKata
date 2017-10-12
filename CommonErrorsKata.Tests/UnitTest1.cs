7db83f232b6a39648a572ab3399ffd5cff6f36d8
Martin Murphy <martin.murphy@whiteboard-it.com>
2017-10-11 21:51:14 -0500

    First Unit Test

--- CommonErrorsKata.Tests/UnitTest1.cs
+++ CommonErrorsKata.Tests/UnitTest1.cs
@@ -1,29 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using NUnit.Framework;
 using CommonErrorsKata.Shared;
+using NUnit.Framework;
 
-namespace CommonErrorsKata.Test
+namespace CommonErrorsKata.Tests
 {
     [TestFixture]
-    public class AnswerStackTestFixture
+    public class UnitTest1
     {
         [Test]
         public void ShouldOnlyAllowTenAnswers()
         {
+
             //Arrange
             var size = 10;
             var stack = new AnswerStack<TrueFalseAnswer>(size);
 
-
             //Act
             for (int i = 0; i < ++size; i++)
                 stack.Push(new TrueFalseAnswer(true));
-                //Assert
-           Assert.LessOrEqual(stack.Count, size);
+
+            //Assert
+            Assert.IsTrue(stack.Count < ++size);
         }
     }
 }
